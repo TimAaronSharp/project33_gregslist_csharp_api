ff9d35d6156d6f7acc3513fe619ba11defc91292
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-04-16 15:05:20 -0600

    Input auth0 credentials

--- wwwroot/env.js
+++ wwwroot/env.js
@@ -1,3 +1,3 @@
-export const domain = 'YOUR'
-export const clientId = 'STUFF'
-export const audience = 'GOES HERE'
+export const domain = 'dev-tujbwv7x02il14en.us.auth0.com'
+export const clientId = 'obtpms2liE9zifUNOMhOQUMokbv50IzI'
+export const audience = 'https://auth.speedwagonfoundation.com'
